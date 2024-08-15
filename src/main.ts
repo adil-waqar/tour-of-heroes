699249fc2e695c0bd4c2422a3951d6c73f8e08d1
Adil Waqar <adil.waqar78@hotmail.com>
2024-08-15 18:32:14 +0500

    feat: display a list

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  bootstrapApplication,
+  provideProtractorTestingSupport,
+} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport()],
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
